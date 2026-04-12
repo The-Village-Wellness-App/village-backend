891e2b3cf26b1856cd4d7b2ed05cfd750c906bff
Tim <tim.maastricht@gmail.com>
2026-04-12 16:44:49 +1000

    feat: create models #3

---
+++ src/models/PainModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const PainSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
+    },
+    location: {
+      type: String,
+      enum: [
+        "head",
+        "neck",
+        "throat",
+        "shoulders",
+        "back",
+        "chest",
+        "abdomen",
+        "arms",
+        "hands",
+        "pelvic_region",
+        "upper_legs",
+        "lower_legs",
+        "feet",
+      ],
+      required: true,
+    },
+    optional_text: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    occurred_at: {
+      type: Date,
+      default: Date.now,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const PainModel = mongoose.model("Pain", PainSchema);
+
+module.exports = {
+  PainSchema,
+  PainModel,
+};
